0533282354e1f070cf1a22a0d6a875caccd52a68
Thuan Ng <ninhthuan82us@gmail.com>
2018-10-17 13:35:09 +0700

    docker-jenkins

--- docker-hello/server.js
+++ docker-hello/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!' + 'LANGUAGE:' + process.env.LANGUAGE + '\n');
+  response.end('Hello World! v1.6' + 'LANGUAGE:' + process.env.LANGUAGE + '\n');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
